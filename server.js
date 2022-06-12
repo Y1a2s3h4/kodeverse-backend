4690b91bdd8ffd0873c354b9365b4ecf73f93794
Y1a2s3h4 <yashpurohit763@gmail.com>
2022-06-12 16:07:08 +0530

    chore: PORT issue solved

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 const express = require("express");
 const app = express();
-const PORT = process.env.PORT5000 || 5000;
+const PORT = process.env.PORT || 5000;
 const mongoose = require("mongoose");
 const Jobs = require("./models/jobs.models");
 const cors = require("cors");
