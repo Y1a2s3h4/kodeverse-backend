6f3c4e76ca557869409b3d9f421f4c20858eb60d
Y1a2s3h4 <yashpurohit763@gmail.com>
2022-08-14 00:22:23 +0530

    feat: added feature for type:both

--- server.js
+++ server.js
@@ -12,107 +12,6 @@ const bot = new Telegraf(process.env.TELEGRAM_BOT_API);
 app.use(express.json());
 app.use(cors());
 
-// mongoose
-//   .connect(process.env.MONGODB_URI, {
-//     useNewUrlParser: true,
-//     useUnifiedTopology: true,
-//   })
-//   .then(() => console.log("Connected to MongoDB"))
-//   .catch((err) => console.log(err));
-
-// app.post("/tweet", async (req, res) => {
-//   const { tweet_body } = req.body;
-//   try {
-//     const data = await rwClient.v1.tweet(tweet_body);
-//     res.send(data);
-//   } catch (error) {
-//     console.log(error);
-//     res.send(error);
-//   }
-// });
-
-// app.get("/get/jobs", async (req, res) => {
-//   try {
-//     const jobsData = await Jobs.find({});
-//     res.json(jobsData).status(200);
-//   } catch (error) {
-//     res.json({ error }).status(400);
-//   }
-// });
-// app.post("/create/job", async (req, res) => {
-//   try {
-//     const {
-//       company_logo,
-//       company_name,
-//       opening_site,
-//       type,
-//       email,
-//       tags,
-//       job,
-//       domain,
-//     } = req.body;
-//     const Job = await Jobs.create({
-//       company_logo,
-//       company_name,
-//       opening_site,
-//       type,
-//       email,
-//       tags,
-//       job,
-//       domain,
-//     });
-//     Job.save();
-//     res.json(Job).status(200);
-//   } catch (error) {
-//     res.json({ error }).status(400);
-//   }
-// });
-// app.patch("/update/job", async (req, res) => {
-//   try {
-//     const {
-//       _id,
-//       company_logo,
-//       company_name,
-//       opening_site,
-//       type,
-//       email,
-//       tags,
-//       job,
-//       domain,
-//     } = req.body;
-//     await Jobs.updateOne(
-//       { _id },
-//       {
-//         company_logo,
-//         company_name,
-//         opening_site,
-//         type,
-//         email,
-//         tags,
-//         job,
-//         domain,
-//       }
-//     );
-//     const jobsData = await Jobs.find({});
-//     res.json(jobsData).status(200);
-//   } catch (error) {
-//     res.json({ error }).status(400);
-//   }
-// });
-// app.delete("/delete/job", async (req, res) => {
-//   try {
-//     const { jobId } = req.query;
-//     await Jobs.deleteOne({ _id: jobId });
-//     const jobsData = await Jobs.find({});
-//     res.json(jobsData).status(200);
-//   } catch (error) {
-//     res.json({ error }).status(400);
-//   }
-// });
-
-// app.listen(PORT, () =>
-//   console.log("🔥 Server Running on http://localhost:" + PORT)
-// );
 const superWizard = new Scenes.WizardScene(
   "super-wizard",
   (ctx) => {
@@ -128,19 +27,26 @@ const superWizard = new Scenes.WizardScene(
         ? "Non-Tech"
         : "Both";
     ctx.session.groups = { Tech: -514549590, "Non-Tech": -514549590 };
+    // ctx.session.groups = { Tech: -1001727141534, "Non-Tech": -1001727141534 };
+    // change community from string to array when type is both, Add both url as array elements
     ctx.session.community =
       ctx.session.type === "Tech"
         ? "https://t.me/kodeverse/"
-        : "https://t.me/kodeverseNT/";
+        : ctx.session.type === "Non-Tech"
+        ? "https://t.me/kodeverseNT/"
+        : "https://t.me/kodeverse/,https://t.me/kodeverse/NT";
     ctx.reply("2) Company Name: ");
     return ctx.wizard.next();
   },
   (ctx) => {
     ctx.session.company_name = ctx.message.text;
+    // think about domain what to do when type is Both.
     ctx.session.domain =
       ctx.session.type === "Tech"
         ? "IT, Software, Product, Engineering, Management"
-        : "HR, Marketing, Operations, Finance, Sales";
+        : ctx.session.type === "Non-Tech"
+        ? "HR, Marketing, Operations, Finance, Sales"
+        : "Tech: IT, Software, Product, Engineering, Management, Non-Tech: HR, Marketing, Operations, Finance, Sales.";
     ctx.reply("3) Job: Check Linkedin Page Job Posts | Thier  Career Site");
     ctx.reply("4) Domain: " + ctx.session.domain);
     ctx.session.job = "Check Linkedin Page Job Posts | Thier  Career Site";
@@ -167,60 +73,155 @@ const superWizard = new Scenes.WizardScene(
       "#portfolio #job #kodeverse #jobs #resume #hiring #share #cv #recruiting #career #" +
       ctx.session.company_name;
     if (ctx.session.message_for === "Telegram") {
-      console.log(ctx.session);
-      bot.telegram.sendMessage(
-        ctx.chat.id,
-        `Company Name: ${ctx.session.company_name}\nType: ${ctx.session.type}\n\nJob: ${ctx.session.job}\nDomain: ${ctx.session.domain}\nEmail: ${ctx.session.email}\n\nJoin Community: ${ctx.session.community}`,
-        {
-          reply_markup: {
-            inline_keyboard: [
-              [
-                { text: "Confirm & Send!", callback_data: "confirm" },
-                { text: "Delete!", callback_data: "delete" },
+      if (ctx.session.type.toLowerCase() !== "both") {
+        bot.telegram.sendMessage(
+          ctx.chat.id,
+          `Company Name: ${ctx.session.company_name}\nType: ${ctx.session.type}\n\nJob: ${ctx.session.job}\nDomain: ${ctx.session.domain}\nEmail: ${ctx.session.email}\n\nJoin Community: ${ctx.session.community}`,
+          {
+            reply_markup: {
+              inline_keyboard: [
+                [
+                  { text: "Confirm & Send!", callback_data: "confirm" },
+                  { text: "Delete!", callback_data: "delete" },
+                ],
               ],
-            ],
-          },
-        }
-      );
+            },
+          }
+        );
+      } else {
+        bot.telegram.sendMessage(
+          ctx.chat.id,
+          `Company Name: ${ctx.session.company_name}\nType: ${"Tech"}\n\nJob: ${
+            ctx.session.job
+          }\nDomain: ${"IT, Software, Product, Engineering, Management"}\nEmail: ${
+            ctx.session.email
+          }\n\nJoin Community: ${ctx.session.community.split(",")[0]}`
+        );
+        bot.telegram.sendMessage(
+          ctx.chat.id,
+          `Company Name: ${
+            ctx.session.company_name
+          }\nType: ${"Non-Tech"}\n\nJob: ${
+            ctx.session.job
+          }\nDomain: ${"HR, Marketing, Operations, Finance, Sales"}\nEmail: ${
+            ctx.session.email
+          }\n\nJoin Community: ${ctx.session.community.split(",")[1]}`
+        );
+        setTimeout(() => {
+          bot.telegram.sendMessage(ctx.chat.id, `Choose Actions From Below!`, {
+            reply_markup: {
+              inline_keyboard: [
+                [
+                  { text: "Confirm & Send!", callback_data: "confirm" },
+                  { text: "Delete!", callback_data: "delete" },
+                ],
+              ],
+            },
+          });
+        }, 1000);
+      }
+      console.log(ctx.session);
     } else if (ctx.session.message_for === "Twitter") {
-      bot.telegram.sendMessage(
-        ctx.chat.id,
-        `Company Name: ${ctx.session.company_name}\nType: ${ctx.session.type}\nJob: ${ctx.session.job}\nDomain: ${ctx.session.domain}\nEmail: ${ctx.session.email}\nJoin Community: ${ctx.session.community}\n${ctx.session.twitter_hashtags}`
-      );
-      ctx.scene.leave();
+      if (ctx.session.type.toLowerCase() !== "both") {
+        bot.telegram.sendMessage(
+          ctx.chat.id,
+          `Company Name: ${ctx.session.company_name}\nType: ${ctx.session.type}\nJob: ${ctx.session.job}\nDomain: ${ctx.session.domain}\nEmail: ${ctx.session.email}\nJoin Community: ${ctx.session.community}\n${ctx.session.twitter_hashtags}`
+        );
+        ctx.scene.leave();
+      } else {
+        bot.telegram.sendMessage(
+          ctx.chat.id,
+          `Company Name: ${ctx.session.company_name}\nType: Tech\nJob: ${ctx.session.job}\nDomain: IT, Software, Product, Engineering, Management.\nEmail: ${ctx.session.email}\nJoin Community: https://t.me/kodeverse/\n${ctx.session.twitter_hashtags}`
+        );
+        bot.telegram.sendMessage(
+          ctx.chat.id,
+          `Company Name: ${ctx.session.company_name}\nType: Non-Tech\nJob: ${ctx.session.job}\nDomain: HR, Marketing, Operations, Finance, Sales.\nEmail: ${ctx.session.email}\nJoin Community: https://t.me/kodeverseNT/\n${ctx.session.twitter_hashtags}`
+        );
+        ctx.scene.leave();
+      }
     } else if (ctx.session.message_for === "Linkedin") {
-      bot.telegram.sendMessage(
-        ctx.chat.id,
-        `Company Name: ${ctx.session.company_name}\nType: ${ctx.session.type}\nJob: ${ctx.session.job}\nDomain: ${ctx.session.domain}\nEmail: ${ctx.session.email}\nJoin Community: ${ctx.session.community}\n${ctx.session.twitter_hashtags}`
-      );
-      ctx.scene.leave();
+      if (ctx.session.type.toLowerCase() !== "both") {
+        bot.telegram.sendMessage(
+          ctx.chat.id,
+          `Company Name: ${ctx.session.company_name}\nType: ${ctx.session.type}\nJob: ${ctx.session.job}\nDomain: ${ctx.session.domain}\nEmail: ${ctx.session.email}\nJoin Community: ${ctx.session.community}\n${ctx.session.twitter_hashtags}`
+        );
+        ctx.scene.leave();
+      } else {
+        bot.telegram.sendMessage(
+          ctx.chat.id,
+          `Company Name: ${ctx.session.company_name}\nType: Tech\nJob: ${ctx.session.job}\nDomain: IT, Software, Product, Engineering, Management.\nEmail: ${ctx.session.email}\nJoin Community: https://t.me/kodeverse/\n${ctx.session.twitter_hashtags}`
+        );
+        bot.telegram.sendMessage(
+          ctx.chat.id,
+          `Company Name: ${ctx.session.company_name}\nType: Non-Tech\nJob: ${ctx.session.job}\nDomain: HR, Marketing, Operations, Finance, Sales.\nEmail: ${ctx.session.email}\nJoin Community: https://t.me/kodeverseNT/\n${ctx.session.twitter_hashtags}`
+        );
+      }
     } else {
-      ctx.reply(
-        `Telegram\n\nCompany Name: ${ctx.session.company_name}\nType: ${ctx.session.type}\n\nJob: ${ctx.session.job}\nDomain: ${ctx.session.domain}\nEmail: ${ctx.session.email}\n\nJoin Community: ${ctx.session.community}`
-      );
+      if (ctx.session.type.toLowerCase() !== "both") {
+        // add here a condition for if type == Both, if true create 6 messages , 3 for tech and 3 for non-tech
+        ctx.reply(
+          `Telegram\n\nCompany Name: ${ctx.session.company_name}\nType: ${ctx.session.type}\n\nJob: ${ctx.session.job}\nDomain: ${ctx.session.domain}\nEmail: ${ctx.session.email}\n\nJoin Community: ${ctx.session.community}`
+        );
 
-      ctx.reply(
-        `Twitter\n\nCompany: ${ctx.session.company_name}\nType: ${ctx.session.type}\nJob: Check Linkedin Job Posts | Career Site\nDomain: ${ctx.session.domain}\nEmail: ${ctx.session.email}\nJoin Us: ${ctx.session.community}\n${ctx.session.twitter_hashtags}`
-      );
+        ctx.reply(
+          `Twitter\n\nCompany: ${ctx.session.company_name}\nType: ${ctx.session.type}\nJob: Check Linkedin Job Posts | Career Site\nDomain: ${ctx.session.domain}\nEmail: ${ctx.session.email}\nJoin Us: ${ctx.session.community}\n${ctx.session.twitter_hashtags}`
+        );
 
-      ctx.reply(
-        `Linkedin\n\nCompany Name: ${ctx.session.company_name}\nType: ${ctx.session.type}\nJob: Check Linkedin Job Posts | Career Site\nDomain: ${ctx.session.domain}\nEmail: ${ctx.session.email}\nJoin Community: ${ctx.session.community}\n${ctx.session.twitter_hashtags}`
-      );
+        ctx.reply(
+          `Linkedin\n\nCompany Name: ${ctx.session.company_name}\nType: ${ctx.session.type}\nJob: Check Linkedin Job Posts | Career Site\nDomain: ${ctx.session.domain}\nEmail: ${ctx.session.email}\nJoin Community: ${ctx.session.community}\n${ctx.session.twitter_hashtags}`
+        );
+      } else {
+        ctx.reply(
+          `Telegram\n\nCompany Name: ${ctx.session.company_name}\nType: Tech\n\nJob: ${ctx.session.job}\nDomain: IT, Software, Product, Engineering, Management.\nEmail: ${ctx.session.email}\n\nJoin Community: https://t.me/kodeverse/`
+        );
+
+        ctx.reply(
+          `Twitter\n\nCompany: ${ctx.session.company_name}\nType: Tech\nJob: Check Linkedin Job Posts | Career Site\nDomain: IT, Software, Product, Engineering, Management.\nEmail: ${ctx.session.email}\nJoin Us: https://t.me/kodeverse/\n${ctx.session.twitter_hashtags}`
+        );
+
+        ctx.reply(
+          `Linkedin\n\nCompany Name: ${ctx.session.company_name}\nType: Tech\nJob: Check Linkedin Job Posts | Career Site\nDomain: IT, Software, Product, Engineering, Management.\nEmail: ${ctx.session.email}\nJoin Community: https://t.me/kodeverse/\n${ctx.session.twitter_hashtags}`
+        );
+        ctx.reply(
+          `Telegram\n\nCompany Name: ${ctx.session.company_name}\nType: Non-Tech\n\nJob: ${ctx.session.job}\nDomain: HR, Marketing, Operations, Finance, Sales.\nEmail: ${ctx.session.email}\n\nJoin Community: https://t.me/kodeverseNT/`
+        );
+
+        ctx.reply(
+          `Twitter\n\nCompany: ${ctx.session.company_name}\nType: Non-Tech\nJob: Check Linkedin Job Posts | Career Site\nDomain: HR, Marketing, Operations, Finance, Sales.\nEmail: ${ctx.session.email}\nJoin Us: https://t.me/kodeverseNT/\n${ctx.session.twitter_hashtags}`
+        );
+
+        ctx.reply(
+          `Linkedin\n\nCompany Name: ${ctx.session.company_name}\nType: Non-Tech\nJob: Check Linkedin Job Posts | Career Site\nDomain: HR, Marketing, Operations, Finance, Sales.\nEmail: ${ctx.session.email}\nJoin Community: https://t.me/kodeverseNT/\n${ctx.session.twitter_hashtags}`
+        );
+      }
     }
     return ctx.wizard.next();
   }
 );
-superWizard.command("cancel", (ctx) => {
+superWizard.command("/restart", (ctx) => {
   ctx.scene.leave();
   ctx.reply("Cancelled!!!");
 });
 superWizard.action("confirm", (ctx) => {
-  bot.telegram.sendMessage(
-    ctx.session.groups[ctx.session.type],
-    `Company Name: ${ctx.session.company_name}\nType: ${ctx.session.type}\n\nJob: ${ctx.session.job}\nDomain: ${ctx.session.domain}\nEmail: ${ctx.session.email}\n\nJoin Community: ${ctx.session.community}`
-  );
-  ctx.reply("Message Send 👍");
-  ctx.scene.leave();
+  if (ctx.session.type.toLowerCase() !== "both") {
+    bot.telegram.sendMessage(
+      ctx.session.groups[ctx.session.type],
+      `Company Name: ${ctx.session.company_name}\nType: ${ctx.session.type}\n\nJob: ${ctx.session.job}\nDomain: ${ctx.session.domain}\nEmail: ${ctx.session.email}\n\nJoin Community: ${ctx.session.community}`
+    );
+    ctx.reply("Message Send 👍");
+    ctx.scene.leave();
+  } else {
+    bot.telegram.sendMessage(
+      ctx.session.groups["Tech"],
+      `Company Name: ${ctx.session.company_name}\nType: Tech\n\nJob: ${ctx.session.job}\nDomain: IT, Software, Product, Engineering, Management.\nEmail: ${ctx.session.email}\n\nJoin Community: https://t.me/kodeverse/`
+    );
+    bot.telegram.sendMessage(
+      ctx.session.groups["Non-Tech"],
+      `Company Name: ${ctx.session.company_name}\nType: Non-Tech\n\nJob: ${ctx.session.job}\nDomain: HR, Marketing, Operations, Finance, Sales.\nEmail: ${ctx.session.email}\n\nJoin Community: https://t.me/kodeverseNT/`
+    );
+    ctx.reply("Message Send 👍");
+    ctx.scene.leave();
+  }
 });
 superWizard.action("delete", (ctx) => {
   ctx.session = {};
